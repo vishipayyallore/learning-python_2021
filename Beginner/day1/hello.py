df6de2fa4cb4241319d2be0043aad377be34ce15
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-05-12 14:04:42 +0530

    Today's Program

---
+++ Beginner/day1/hello.py
@@ -0,0 +1,14 @@
+# First Python Program
+
+print('Please enter your Name: ')
+name = input()
+
+print('What is your age: ');
+age = input()
+
+print('User Details ...')
+print('Name: ', name);
+print('Name Length: ', len(name))
+print('Age: ', age)
+
+
