5e4183049fbe33a48fec111e3a6bb5c2677a0f83
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-15 23:57:06 +0530

    Create main.py

---
+++ 2021/day1/main.py
@@ -0,0 +1,13 @@
+print("Hello ", input("What is your name? "))
+
+print(len(input("What is your name? ")))
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
