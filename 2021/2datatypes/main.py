b4e27b2e331ad6fbc411025f25199bd919921b2d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-18 19:49:55 +0530

    Update main.py

--- 2021/2datatypes/main.py
+++ 2021/2datatypes/main.py
@@ -3,3 +3,28 @@
 # print(len(125))
 # print("Hello" + 45)
 
+two_digit_number = input('Enter 2 digit number: ')
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+print( 8 % 5)
+
+# PEMDAS () | ** | * / | + - 
+# ** Exponent
+# // Integer Division
+# % Modulus
+
+# Data Types
+print(type('Sri Varu'))
+print(type("Sri Varu"))
+print(type('''Sri Varu'''))
+print(type("""Sri Varu"""))
+
+print(type(124))
+
+print(type(124.34))
+
+print(type(True))
+print(type(False))
+
+print(type(None))
+
